73829daf8cb2819a27c411a24b9686cf93d2517c
israel <israelpadonu14@gmail.com>
2024-09-21 23:03:28 +0100

    .

--- src/components/hamsterCard.tsx
+++ src/components/hamsterCard.tsx
@@ -1,9 +1,52 @@
-function hamsterCard() {
-    return (
-        <div>
-            <h2>hamsterCard</h2>
-        </div>
-    )
-}
+import React from "react";
+import { dollarCoin } from "../images";
 
-export default hamsterCard;
+
+
+const HamsterCard: React.FC = () => {
+  return (
+    <div className="w-[95%] rounded-xl my-4 mx-auto flex flex-col text-nowrap bg-gray-600 p-1  text-center col-span-1">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img
+          className="w-10/12 mt-6 rounded-lg h-24 mx-auto"
+          src="https://v1.tailwindcss.com/img/card-top.jpg"
+          alt="Sunset in the mountains"
+        />
+        <div className="mx-auto py-4 text-center flex flex-col items-center gap-2">
+          <p className="font-bold text-xs mb-2">The Coldest Sunset</p>
+          <p className="text-slate-400 text-xs font-extrabold text-wrap">
+            Lorem ipsum dolor
+          </p>
+          <p className="text-slate-400 text-xs flex items-center gap-2">
+            Profit per hour{" "}
+            <span className="flex items-center">
+              {" "}
+              <img
+                src={dollarCoin}
+                alt="Dollar Coin"
+                className="w-[18px] h-[18px]"
+              />
+              +4.3k
+            </span>
+          </p>
+        </div>
+        <hr className="opacity-25" />
+        <div className="px-6 pt-4 pb-2 flex items-center justify-between">
+          <span>lvl 13</span>
+          <hr className="rotate-90" />
+          <span className="flex items-center">
+            {" "}
+            <img
+              src={dollarCoin}
+              alt="Dollar Coin"
+              className="w-[18px] h-[18px]"
+            />
+            151.3k
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HamsterCard;
--- src/pages/Mine.tsx
+++ src/pages/Mine.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { dollarCoin } from "../images";
 import formatProfitPerHour from "../utils/formatProfitPerHour";
 import calculateTimeLeft from "../utils/calculateTimeLeft";
+import HamsterCard from "../components/hamsterCard";
 interface Props {
   points: number;
   coins: number;
@@ -27,10 +28,10 @@ const Mine: React.FC<Props> = (props) => {
   }, []);
   return (
     <>
-      <div className="bg-black flex justify-center  overflow-scroll">
-        <div className="w-full mx-auto bg-black text-white h-screen font-bold flex flex-col max-w-xl">
+      <div className="bg-black flex justify-center max-h-screen overflow-scroll ">
+        <div className="w-full mx-auto bg-black text-white h-screen  max-h-screen overflow-scroll font-bold flex flex-col max-w-xl">
           <div className="flex-grow   mt-20 bg-[#f3ba2f] rounded-t-[48px] relative top-glow z-0 ">
-            <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025] rounded-t-[46px]">
+            <div className="absolute top-[2px] left-0 right-0 bottom-0 bg-[#1d2025]  max-h-screen  rounded-t-[46px]">
               <section className="flex justify-evenly   !text-nowrap mx-auto my-8 gap-1">
                 <div className="flex flex-col   bg-gray-700 rounded-lg text-xs gap-1 px-4 py-2 mx-auto justify-between items-center">
                   <span className="text-[#f3ba2f] mb-2">Earn per tap</span>
@@ -103,7 +104,6 @@ const Mine: React.FC<Props> = (props) => {
                         <a
                           className="z-30 flex items-center justify-center w-full  text-sm mb-0 transition-all ease-in-out border-0 rounded-md cursor-pointer   bg-black/10 p-4"
                           role="tab"
-                          
                         >
                           Markets
                         </a>
@@ -141,6 +141,12 @@ const Mine: React.FC<Props> = (props) => {
                     </ul>
                   </div>
                 </div>
+              </section>
+              <section className="grid grid-cols-2 w-[95%] mx-auto ">
+                <HamsterCard />
+                <HamsterCard />
+                <HamsterCard />
+                <HamsterCard />
               </section>
             </div>
           </div>
